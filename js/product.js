3c1b7e1fba2f1ae3ba44bbc88b2c742f92446073
your name <manhnguyen.tla@gmail.com>
2022-09-11 20:57:01 +0700

    u1

--- js/product.js
+++ js/product.js
@@ -4,17 +4,19 @@ class item{
     name;
     vote;
     price;
+    cata;
     quanlity;
-    total;
+    total
 
-    constructor(discount,image,name,vote,price,quanlity,total){
+    constructor(discount,image,name,vote,price,cata,quanlity,total){
         this.discount=discount;
         this.image=image;
         this.name=name;
         this.vote=vote;
         this.price=price;
+        this.cata=cata;
         this.quanlity=quanlity;
-        this.total=total
+        this.total=total;
     }
     getprice(){
         return this.quanlity*this.price;
